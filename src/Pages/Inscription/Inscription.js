05591d1466790bf53b589c60a03d6e48b4affe4b
_luisfds_ <luisnice2005@gmail.com>
2024-11-19 10:42:02 +0100

    Formulaire d'inscription a jour

--- src/Pages/Inscription/Inscription.js
+++ src/Pages/Inscription/Inscription.js
@@ -30,6 +30,8 @@ function Inscription() {
             content: `Nouvelle inscription:\nNom: ${formData.lastName}\nPrénom: ${formData.firstName}\nEmail: ${formData.email}\nMotivation: ${formData.motivation}\nDate de naissance: ${formData.birthDate}`
         };
 
+        let success = true;
+
         try {
             const response = await fetch(webhookURL, {
                 method: 'POST',
@@ -39,10 +41,8 @@ function Inscription() {
                 body: JSON.stringify(message)
             });
 
-            if (response.ok) {
-                console.log('Données envoyées avec succès à Discord');
-            } else {
-                console.error('Erreur lors de l\'envoi des données à Discord');
+            if (!response.ok) {
+                success = false;
             }
             
             const emailData = {
@@ -54,9 +54,21 @@ function Inscription() {
             };
 
             await emailjs.send('service_knzqf1l', 'template_og0qfap', emailData);
-            console.log('E-mail envoyé avec succès');
         } catch (error) {
-            console.error('Erreur:', error);
+            success = false;
+        }
+
+        if (success) {
+            alert('Données envoyées avec succès');
+            setFormData({
+                firstName: '',
+                lastName: '',
+                birthDate: '',
+                email: '',
+                motivation: ''
+            });
+        } else {
+            alert('Problème lors de l\'envoi des données');
         }
     };
 
@@ -65,7 +77,7 @@ function Inscription() {
             <div className='title-container'>
                 <div className='title'>
                     <h1>Inscription</h1>
-                    <span className="emoji">💻</span>
+                    <span className="emoji">📜</span>
                 </div>
             </div>
             <div className='form-container'>
@@ -134,132 +146,3 @@ function Inscription() {
 
 export default Inscription;
 
-
-
-
-
-
-
-
-
-// function Inscription() {
-//     const [formData, setFormData] = useState({
-//         firstName: '',
-//         lastName: '',
-//         birthDate: '',
-//         email: '',
-//         motivation: ''
-//     });
-
-//     const handleChange = (e) => {
-//         const { name, value } = e.target;
-//         setFormData({
-//             ...formData,
-//             [name]: value
-//         });
-//     };
-
-//     const handleSubmit = async (e) => {
-//         e.preventDefault();
-        
-//         const webhookURL = 'https://discord.com/api/webhooks/1295433379780755547/JLjadxLQK0FjOLl_a6FjOdRcRI_NWgJQETnb0nEgtsXhTa0MbEE9dULrDjtee9B1Mnu_';
-
-//         const message = {
-//             content: `Nouvelle inscription:\nNom: ${formData.lastName}\nPrénom: ${formData.firstName}\nEmail: ${formData.email}\nMotivation: ${formData.motivation}\nDate de naissance: ${formData.birthDate}`
-//         };
-
-//         try {
-//             const response = await fetch(webhookURL, {
-//                 method: 'POST',
-//                 headers: {
-//                     'Content-Type': 'application/json'
-//                 },
-//                 body: JSON.stringify(message)
-//             });
-
-//             if (response.ok) {
-//                 console.log('Données envoyées avec succès à Discord');
-//             } else {
-//                 console.error('Erreur lors de l\'envoi des données à Discord');
-//             }
-
-//             await emailjs.send('service_knzqf1l', 'template_og0qfap', formData, 'p_iVuB_xWxIJj-eY0');
-//             console.log('E-mail envoyé avec succès');
-//         } catch (error) {
-//             console.error('Erreur:', error);
-//         }
-//     };
-
-//     return (
-//         <div className='Inscription'>
-//             <div className='title-container'>
-//                 <div className='title'>
-//                     <h1>Inscription</h1>
-//                     <span className="emoji">💻</span>
-//                 </div>
-//             </div>
-//             <div className='form-container'>
-//                 <form onSubmit={handleSubmit}>
-//                     <div>
-//                         <label htmlFor="firstName">Prénom:</label>
-//                         <input
-//                             type="text"
-//                             id="firstName"
-//                             name="firstName"
-//                             value={formData.firstName}
-//                             onChange={handleChange}
-//                             required
-//                         />
-//                     </div>
-//                     <div>
-//                         <label htmlFor="lastName">Nom:</label>
-//                         <input
-//                             type="text"
-//                             id="lastName"
-//                             name="lastName"
-//                             value={formData.lastName}
-//                             onChange={handleChange}
-//                             required
-//                         />
-//                     </div>
-//                     <div>
-//                         <label htmlFor="birthDate">Date de naissance:</label>
-//                         <input
-//                             type="date"
-//                             id="birthDate"
-//                             name="birthDate"
-//                             value={formData.birthDate}
-//                             onChange={handleChange}
-//                             required
-//                         />
-//                     </div>
-//                     <div>
-//                         <label htmlFor="email">Email:</label>
-//                         <input
-//                             type="email"
-//                             id="email"
-//                             name="email"
-//                             value={formData.email}
-//                             onChange={handleChange}
-//                             required
-//                         />
-//                     </div>
-//                     <div>
-//                         <label htmlFor="motivation">Lettre de motivation:</label>
-//                         <textarea
-//                             id="motivation"
-//                             name="motivation"
-//                             value={formData.motivation}
-//                             onChange={handleChange}
-//                             required
-//                             minLength={50}
-//                         />
-//                     </div>
-//                     <button type="submit">S'inscrire</button>
-//                 </form>
-//             </div>
-//         </div>
-//     );
-// }
-
-// export default Inscription;
